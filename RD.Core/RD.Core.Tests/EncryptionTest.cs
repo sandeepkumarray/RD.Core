68cf0ceb7620c36a22800bd1c5f65e04ccf9c244
sandeep <sandeepray2075@gmail.com>
2019-06-15 12:56:21 +0530

    Initial commit

---
+++ RD.Core/RD.Core.Tests/EncryptionTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RD.Core.Tests
+{
+    class EncryptionTest
+    {
+        [Test]
+        public void EncryptTest()
+        {
+
+            //arrange
+            //act
+            //assert
+            Assert.AreEqual(1,"2");
+        }
+    }
+}
